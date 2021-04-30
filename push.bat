fc22622352fdf8e593a096d5c2aa54582c1021d2
hunsou <hunsou@msn.com>
2021-05-01 01:59:01 +0800

    2021-05-01  1:59:01

--- push.bat
+++ push.bat
@@ -6,5 +6,5 @@ git commit -m "%date:~6,4%-%date:~0,2%-%date:~3,2% %time:~0,8%"
 rem git push origin master --tags
 :gitpush
 git push origin master
-IF ERRORLEVEL 1 got gitpush
+IF ERRORLEVEL 1 goto gitpush
 if %errorlevel%==1 goto gitpush
